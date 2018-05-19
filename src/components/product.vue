e34740c2510460a882c7204d21cb50f917950715
Jinygle <36870352+Jinygle@users.noreply.github.com>
2018-05-19 09:30:01 +0800

    增加商品

---
+++ src/components/product.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    products
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
--- src/views/list.vue
+++ src/views/list.vue
@@ -19,17 +19,30 @@
             :key="item"
             @click="handleFilterColor(item)">{{item}}</span>
         </div>
+        <div class="list-control-order">
+          <span>排序：</span>
+          <span class="list-control-order-item"
+                :class="{ on: order === '' }">默认</span>
+          <span class="list-control-order-item">销量</span>
+          <span class="list-control-order-item">价格</span>
+        </div>
       </div>
+      <Product></Product>
   </div>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import Product from '../components/product'
 
 export default {
+  components: {
+    Product
+  },
   data () {
     return {
       filterBrand: '',
-      filterColor: ''
+      filterColor: '',
+      order: ''
     }
   },
   computed: {
@@ -61,7 +74,8 @@ export default {
   .list-control-filter{
     margin-bottom: 16px;
   }
-  .list-control-filter-item{
+  .list-control-filter-item,
+  .list-control-order-item{
     cursor: pointer;
     border: 1px solid #e9eaec;
     border-radius: 4px;
